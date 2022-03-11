11d0899a369e3b948362f3b32ebe2b8a019d6a15
hotaryuzaki <hotaru_edogawa@yahoo.com>
2022-03-11 20:27:12 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const url = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&titles=pizza';
+const word = 'pizza';
+
+async function main(word) {
+  try {
+    const regWord = `\\b${word}\\b`;
+    const specificWord = new RegExp(regWord,"g");
+    const getData = await axios.get(url);
+    const getDataKey = Object.keys(getData.data.query.pages)
+    const getDataStr = getData.data.query.pages[getDataKey].extract;
+    const wordCounter = countWord(getDataStr, specificWord);
+    return wordCounter;
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function countWord(str, find) {
+  return (str.split(find)).length - 1;
+}
+
+const lari = new Promise((resolve, reject) => {
+  resolve(main(word));
+}).then((kata) => {
+  console.log(`kata ${word} ada sebanyak`, kata);
+  return kata;
+});
